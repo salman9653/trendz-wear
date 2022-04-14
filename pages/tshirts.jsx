6bf5e93398f814e53727a7f7d55b383807b6503a
Salman <salman9915189734@gmail.com>
2022-04-15 01:37:02 +0530

    tshirt page added

--- pages/tshirts.jsx
+++ pages/tshirts.jsx
@@ -1,10 +1,148 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+
 
 const tshirts = () => {
     return (
-        <div>
-            T-shirts
-        </div>
+        <section className="text-gray-600 body-font">
+            <div className="container px-5 py-24 mx-auto">
+                <div className="flex flex-wrap -m-4 ">
+                    <Link href={"/product/"}>
+                        <div className="lg:w-1/5 md:w-1/3 p-4 w-full cursor-pointer">
+                            <a className="block relative h-60 overflow-hidden rounded-t-xl border-8 border-slate-200">
+                                <img alt="ecommerce" className=" rounded-md object-cover object-center m-auto h-full block" src="https://m.media-amazon.com/images/I/61404Prm85L._UX679_.jpg" />
+                            </a>
+                            <div className="bg-slate-200 p-4 rounded-b-xl text-center md:text-left">
+                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Wrist Watch</h3>
+                                <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                                <p className="mt-1">₹ 699/- </p>
+                                <button className='bg-green-600 mt-2 text-gray-200 rounded w-full py-1'>Add to Cart <AiOutlineShoppingCart className='text-gray-200 ml-2 inline text-right' /></button>
+                            </div>
+                        </div>
+                    </Link>
+                    <Link href={"/product/"}>
+                        <div className="lg:w-1/5 md:w-1/3 p-4 w-full cursor-pointer">
+                            <a className="block relative h-60 overflow-hidden rounded-t-xl border-8 border-slate-200">
+                                <img alt="ecommerce" className=" rounded-md object-cover object-center m-auto h-full block" src="https://m.media-amazon.com/images/I/61404Prm85L._UX679_.jpg" />
+                            </a>
+                            <div className="bg-slate-200 p-4 rounded-b-xl text-center md:text-left">
+                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Wrist Watch</h3>
+                                <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                                <p className="mt-1">₹ 699/- </p>
+                                <button className='bg-green-600 mt-2 text-gray-200 rounded w-full py-1'>Add to Cart <AiOutlineShoppingCart className='text-gray-200 ml-2 inline text-right' /></button>
+                            </div>
+                        </div>
+                    </Link>
+                    <Link href={"/product/"}>
+                        <div className="lg:w-1/5 md:w-1/3 p-4 w-full cursor-pointer">
+                            <a className="block relative h-60 overflow-hidden rounded-t-xl border-8 border-slate-200">
+                                <img alt="ecommerce" className=" rounded-md object-cover object-center m-auto h-full block" src="https://m.media-amazon.com/images/I/61404Prm85L._UX679_.jpg" />
+                            </a>
+                            <div className="bg-slate-200 p-4 rounded-b-xl text-center md:text-left">
+                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Wrist Watch</h3>
+                                <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                                <p className="mt-1">₹ 699/- </p>
+                                <button className='bg-green-600 mt-2 text-gray-200 rounded w-full py-1'>Add to Cart <AiOutlineShoppingCart className='text-gray-200 ml-2 inline text-right' /></button>
+                            </div>
+                        </div>
+                    </Link>
+                    <Link href={"/product/"}>
+                        <div className="lg:w-1/5 md:w-1/3 p-4 w-full cursor-pointer">
+                            <a className="block relative h-60 overflow-hidden rounded-t-xl border-8 border-slate-200">
+                                <img alt="ecommerce" className=" rounded-md object-cover object-center m-auto h-full block" src="https://m.media-amazon.com/images/I/61404Prm85L._UX679_.jpg" />
+                            </a>
+                            <div className="bg-slate-200 p-4 rounded-b-xl text-center md:text-left">
+                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Wrist Watch</h3>
+                                <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                                <p className="mt-1">₹ 699/- </p>
+                                <button className='bg-green-600 mt-2 text-gray-200 rounded w-full py-1'>Add to Cart <AiOutlineShoppingCart className='text-gray-200 ml-2 inline text-right' /></button>
+                            </div>
+                        </div>
+                    </Link>
+                    <Link href={"/product/"}>
+                        <div className="lg:w-1/5 md:w-1/3 p-4 w-full cursor-pointer">
+                            <a className="block relative h-60 overflow-hidden rounded-t-xl border-8 border-slate-200">
+                                <img alt="ecommerce" className=" rounded-md object-cover object-center m-auto h-full block" src="https://m.media-amazon.com/images/I/61404Prm85L._UX679_.jpg" />
+                            </a>
+                            <div className="bg-slate-200 p-4 rounded-b-xl text-center md:text-left">
+                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Wrist Watch</h3>
+                                <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                                <p className="mt-1">₹ 699/- </p>
+                                <button className='bg-green-600 mt-2 text-gray-200 rounded w-full py-1'>Add to Cart <AiOutlineShoppingCart className='text-gray-200 ml-2 inline text-right' /></button>
+                            </div>
+                        </div>
+                    </Link>
+                    <Link href={"/product/"}>
+                        <div className="lg:w-1/5 md:w-1/3 p-4 w-full cursor-pointer">
+                            <a className="block relative h-60 overflow-hidden rounded-t-xl border-8 border-slate-200">
+                                <img alt="ecommerce" className=" rounded-md object-cover object-center m-auto h-full block" src="https://m.media-amazon.com/images/I/61404Prm85L._UX679_.jpg" />
+                            </a>
+                            <div className="bg-slate-200 p-4 rounded-b-xl text-center md:text-left">
+                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Wrist Watch</h3>
+                                <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                                <p className="mt-1">₹ 699/- </p>
+                                <button className='bg-green-600 mt-2 text-gray-200 rounded w-full py-1'>Add to Cart <AiOutlineShoppingCart className='text-gray-200 ml-2 inline text-right' /></button>
+                            </div>
+                        </div>
+                    </Link>
+                    <Link href={"/product/"}>
+                        <div className="lg:w-1/5 md:w-1/3 p-4 w-full cursor-pointer">
+                            <a className="block relative h-60 overflow-hidden rounded-t-xl border-8 border-slate-200">
+                                <img alt="ecommerce" className=" rounded-md object-cover object-center m-auto h-full block" src="https://m.media-amazon.com/images/I/61404Prm85L._UX679_.jpg" />
+                            </a>
+                            <div className="bg-slate-200 p-4 rounded-b-xl text-center md:text-left">
+                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Wrist Watch</h3>
+                                <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                                <p className="mt-1">₹ 699/- </p>
+                                <button className='bg-green-600 mt-2 text-gray-200 rounded w-full py-1'>Add to Cart <AiOutlineShoppingCart className='text-gray-200 ml-2 inline text-right' /></button>
+                            </div>
+                        </div>
+                    </Link>
+                    <Link href={"/product/"}>
+                        <div className="lg:w-1/5 md:w-1/3 p-4 w-full cursor-pointer">
+                            <a className="block relative h-60 overflow-hidden rounded-t-xl border-8 border-slate-200">
+                                <img alt="ecommerce" className=" rounded-md object-cover object-center m-auto h-full block" src="https://m.media-amazon.com/images/I/61404Prm85L._UX679_.jpg" />
+                            </a>
+                            <div className="bg-slate-200 p-4 rounded-b-xl text-center md:text-left">
+                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Wrist Watch</h3>
+                                <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                                <p className="mt-1">₹ 699/- </p>
+                                <button className='bg-green-600 mt-2 text-gray-200 rounded w-full py-1'>Add to Cart <AiOutlineShoppingCart className='text-gray-200 ml-2 inline text-right' /></button>
+                            </div>
+                        </div>
+                    </Link>
+                    <Link href={"/product/"}>
+                        <div className="lg:w-1/5 md:w-1/3 p-4 w-full cursor-pointer">
+                            <a className="block relative h-60 overflow-hidden rounded-t-xl border-8 border-slate-200">
+                                <img alt="ecommerce" className=" rounded-md object-cover object-center m-auto h-full block" src="https://m.media-amazon.com/images/I/61404Prm85L._UX679_.jpg" />
+                            </a>
+                            <div className="bg-slate-200 p-4 rounded-b-xl text-center md:text-left">
+                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Wrist Watch</h3>
+                                <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                                <p className="mt-1">₹ 699/- </p>
+                                <button className='bg-green-600 mt-2 text-gray-200 rounded w-full py-1'>Add to Cart <AiOutlineShoppingCart className='text-gray-200 ml-2 inline text-right' /></button>
+                            </div>
+                        </div>
+                    </Link>
+                    <Link href={"/product/"}>
+                        <div className="lg:w-1/5 md:w-1/3 p-4 w-full cursor-pointer">
+                            <a className="block relative h-60 overflow-hidden rounded-t-xl border-8 border-slate-200">
+                                <img alt="ecommerce" className=" rounded-md object-cover object-center m-auto h-full block" src="https://m.media-amazon.com/images/I/61404Prm85L._UX679_.jpg" />
+                            </a>
+                            <div className="bg-slate-200 p-4 rounded-b-xl text-center md:text-left">
+                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Wrist Watch</h3>
+                                <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                                <p className="mt-1">₹ 699/- </p>
+                                <button className='bg-green-600 mt-2 text-gray-200 rounded w-full py-1'>Add to Cart <AiOutlineShoppingCart className='text-gray-200 ml-2 inline text-right' /></button>
+                            </div>
+                        </div>
+                    </Link>
+
+
+                </div>
+            </div>
+        </section>
     )
 }
 
